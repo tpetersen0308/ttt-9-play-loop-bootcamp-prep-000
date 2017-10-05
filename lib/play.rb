6097a10aa17684a007a28966f604a6e91b81fe96
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-05 22:33:19 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,20 +23,14 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board, current_player)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index)
     display_board(board)
-    if current_player == "X"
-      current_player == "O"
-    else
-      current_player == "X"
-    end
-  else
-    turn(board, current_player)
+    turn(board)
   end
 end
 
