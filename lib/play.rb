736f6f25be17849f3d8b24e9dbb996c0c99d6686
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-05 22:28:48 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,13 +23,18 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
+def turn(board, token)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, token)
     display_board(board)
+    if token == "X"
+      token == "O"
+    else 
+      token == "X"
+    end
   else
     turn(board)
   end
