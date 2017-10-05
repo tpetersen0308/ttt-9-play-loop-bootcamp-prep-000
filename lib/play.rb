011afe7bb1521592078f7f441b8cc71da8b1c914
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-05 22:21:19 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,15 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  counter = 0
+  until counter == 9
+    turn(board)
+    counter += 1
+    if counter % 2 == 0
+      token = "X"
+    else
+      token = "O"
+    end
+  end
+end
